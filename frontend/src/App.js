de6a32711f0d5cd7f6d9044d47f4387dd5622927
Matheus Amendoeira <mathamenrj@gmail.com>
2024-08-07 19:39:55 -0300

    small state control of the 2 pages added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar.jsx';
@@ -5,15 +6,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import Pedidos from './pages/pedidos/pedidos';
 function App() {
+    const [states, setStatus] = useState({ home: 'online', pedidos: 'online' });
+
+  useEffect(() => {
+    fetch('/states.json')
+      .then(response => response.json())
+      .then(data => setStatus(data))
+      .catch(error => console.error('Error fetching status:', error));
+  }, []);
+
   return (
     <div>
-    <Navbar />
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/pedidos" element={<Pedidos />} />
-    </Routes>
-  </div>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={states.home === 'online' ? <Home /> : <div>Home is offline</div>} />
+        <Route path="/pedidos" element={states.pedidos === 'online' ? <Pedidos /> : <div>Pedidos is offline</div>} />
+      </Routes>
+    </div>
   );
+  
 }
 
 export default App;
