f409daeea7532a3397a71039031205e108e784f0
JoelRTavares <108430269+JoelRTavares@users.noreply.github.com>
2024-09-04 17:25:37 -0300

    Usando Mediator para Create item

--- frontend/src/pages/Items/CreateItemPage.jsx
+++ frontend/src/pages/Items/CreateItemPage.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import logo from './../../images/logo2.png';
 import CardProduto from "../../Components/ItemCardapio/CardProduto";
+import { Mediator } from "../../Mediator";
 
 export default function CreateItemPage()
 {
@@ -8,15 +9,33 @@ export default function CreateItemPage()
     const[description, setDescription] = useState("");
     const[price, setPrice] = useState(0);
     const[photo, setPhoto] = useState(logo); //Ja deixando como photo a nossa logo padrao
-    const[cnpj_restaurant, setCNPJ] = useState("");//Aqui tambem seria bom deixar padrao o cnpj do restaurante
+    const[cnpj_restaurant, setCNPJ] = useState("11-111-11/0001-1");//Aqui tambem seria bom deixar padrao o cnpj do restaurante
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        alert('Produto criado com sucesso (mas no back nao rola nada)')
-        setName('');
-        setDescription('');
-        setPrice(0);
+        const newItem = {
+            name,
+            description,
+            price,
+            photo,
+            cnpj_restaurant
+        };
 
+        const response = await Mediator.add_item(newItem);
+        
+        if (response === Mediator.INVALID_SESSION) {
+            console.error("Sessão inválida. Faça login novamente.");
+        } 
+        else if (response === Mediator.ALREADY_EXISTS) {
+            console.error("Este item já existe.");
+        } 
+        else {
+        // Item criado com sucesso, você pode limpar o formulário ou redirecionar o usuário
+            alert("Item criado com sucesso:", response);
+            setName('');
+            setDescription('');
+            setPrice('');
+        }
     }
     return(
         <div className="container">
